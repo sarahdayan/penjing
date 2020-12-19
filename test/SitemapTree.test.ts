f365531ecc7479c442c97df5aa1d4e279968705f
Sarah Dayan <dayan.sarah@gmail.com>
2020-12-20 00:07:05 +0100

    feat: introduce SitemapTree class

---
+++ test/SitemapTree.test.ts
@@ -0,0 +1,99 @@
+import mock from 'mock-fs';
+
+import { SitemapTree, Resource } from '../src';
+
+let tree: SitemapTree | null = null;
+
+beforeAll(() => {
+  mock({
+    path: {
+      'first.txt': 'Some content.',
+      'second.txt': 'Some other content.',
+    },
+  });
+});
+
+afterEach(() => {
+  tree = null;
+});
+
+afterAll(() => {
+  mock.restore();
+});
+
+describe('SitemapTree', () => {
+  describe('initialization', () => {
+    beforeEach(() => {
+      tree = new SitemapTree(null);
+    });
+    it('returns a null resource', () => {
+      expect(tree!.resource).toBeNull();
+    });
+    it('returns a null parent', () => {
+      expect(tree!.parent).toBeNull();
+    });
+    it('returns empty children', () => {
+      expect(tree!.children).toEqual([]);
+    });
+    it('returns a null URL part', () => {
+      expect(tree!.urlPart).toBeNull();
+    });
+    it('returns a null URL', () => {
+      expect(tree!.url).toBeNull();
+    });
+  });
+  describe('#add', () => {
+    beforeEach(() => {
+      tree = new SitemapTree(null);
+
+      tree!.add(new Resource('path/first.txt'));
+      tree!.add(new Resource('path/second.txt'));
+    });
+    it('recursively adds the first level of the resource to the tree', () => {
+      const child = tree!.children[0];
+
+      expect(child.resource).toBeNull();
+
+      expect(child.parent!.children).toHaveLength(1);
+      expect(child.parent!.resource).toBeNull();
+      expect(child.parent!.urlPart).toBeNull();
+      expect(child.parent!.url).toBeNull();
+
+      expect(child.children).toHaveLength(2);
+      expect(child.urlPart).toBe('path');
+      expect(child.url).toBe('path');
+    });
+    it('recursively adds the first child of the second level of the resource to the tree', () => {
+      const [child] = tree!.children[0].children;
+
+      expect(child.resource!.source).toBe('path/first.txt');
+      expect(child.resource!.destination).toBe('/path/first/');
+      expect(child.resource!.data).toBe('Some content.');
+
+      expect(child.parent!.children).toHaveLength(2);
+      expect(child.parent!.resource).toBeNull();
+      expect(child.parent!.urlPart).toBe('path');
+      expect(child.parent!.url).toBe('path');
+
+      expect(child.children).toEqual([]);
+      expect(child.urlPart).toBe('first');
+      expect(child.url).toBe('path/first');
+    });
+    it('recursively adds the second child of the second level of the resource to the tree', () => {
+      const [, child] = tree!.children[0].children;
+
+      expect(child.resource!.source).toBe('path/second.txt');
+      expect(child.resource!.destination).toBe('/path/second/');
+      expect(child.resource!.data).toBe('Some other content.');
+
+      expect(child.parent!.children).toHaveLength(2);
+      expect(child.parent!.resource).toBeNull();
+      expect(child.parent!.urlPart).toBe('path');
+      expect(child.parent!.url).toBe('path');
+
+      expect(child.children).toEqual([]);
+      expect(child.urlPart).toBe('second');
+      expect(child.url).toBe('path/second');
+    });
+  });
+});
