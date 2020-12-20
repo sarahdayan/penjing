3188c95df5dcc18ae9e5622a14323c36aea0661a
Sarah Dayan <dayan.sarah@gmail.com>
2020-12-20 02:04:05 +0100

    refactor: use getters to keep members private and avoid mutations

--- src/SitemapTree.ts
+++ src/SitemapTree.ts
@@ -1,21 +1,69 @@
 import { Resource } from '.';
 
 export class SitemapTree {
-  resource: Resource | null;
-  parent: SitemapTree | null;
-  children: SitemapTree[];
-  url: string | null;
-  urlPart;
-
+  private _resource: Resource | null;
+  private _parent: SitemapTree | null;
+  private _children: SitemapTree[];
+  private _url: string | null;
+  private _urlPart;
   private urls: Record<string, SitemapTree>;
 
   constructor(urlPart: string | null, urls?: Record<string, SitemapTree>) {
-    this.resource = null;
-    this.parent = null;
-    this.children = [];
-    this.url = null;
+    this._resource = null;
+    this._parent = null;
+    this._children = [];
+    this._url = null;
     this.urls = urls || {};
-    this.urlPart = urlPart;
+    this._urlPart = urlPart;
+  }
+
+  /**
+   * The attached resource.
+   */
+  get resource() {
+    return this._resource;
+  }
+
+  /**
+   * The parent of the sitemap tree.
+   */
+  get parent() {
+    return this._parent;
+  }
+
+  /**
+   * The children of the sitemap tree.
+   */
+  get children() {
+    return this._children;
+  }
+
+  /**
+   * The URL of the sitemap tree.
+   */
+  get url() {
+    return this._url;
+  }
+
+  /**
+   * The URL part of the sitemap tree.
+   */
+  get urlPart() {
+    return this._urlPart;
+  }
+
+  /**
+   * The siblings of the sitemap tree, including itself.
+   */
+  get siblings() {
+    return this._parent?._children || [];
+  }
+
+  /**
+   * All URLs in the sitemap tree.
+   */
+  get allUrls() {
+    return this.urls;
   }
 
   /**
@@ -38,43 +86,29 @@ export class SitemapTree {
     return this.urls[url];
   }
 
-  /**
-   * Retrieve all siblings.
-   */
-  get siblings() {
-    return this.parent?.children || [];
-  }
-
-  /**
-   * Retrieve all URLs in the sitemap tree.
-   */
-  get allUrls() {
-    return this.urls;
-  }
-
   private addParts(parts: string[], usedParts: string[], resource: Resource) {
     if (parts.length <= 0) {
-      this.resource = resource;
+      this._resource = resource;
 
       return;
     }
 
     const [currentPart, ...otherParts] = parts;
     const newUsedParts = [...usedParts, currentPart];
-    const matchingChild = this.children.filter(
-      child => child.urlPart === currentPart
+    const matchingChild = this._children.filter(
+      child => child._urlPart === currentPart
     );
 
     let child = matchingChild.length ? matchingChild[0] : null;
 
     if (!child) {
       child = new SitemapTree(currentPart, this.urls);
-      child.parent = this;
-      child.url = newUsedParts.join('/');
+      child._parent = this;
+      child._url = newUsedParts.join('/');
 
-      this.urls[child.url] = child;
+      this.urls[child._url] = child;
 
-      this.children.push(child);
+      this._children.push(child);
     }
 
     child!.addParts(otherParts, newUsedParts, resource);
