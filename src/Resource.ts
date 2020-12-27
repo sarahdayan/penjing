d210d1bbc9cd200e0e7b0118c7e0cfa838215278
Sarah Dayan <dayan.sarah@gmail.com>
2020-12-27 22:08:19 +0100

    feat: add static methods (#9)

--- src/Resource.ts
+++ src/Resource.ts
@@ -17,13 +17,36 @@ function getFileData(filePath: string) {
 }
 
 export class Resource {
-  source;
-  destination;
-  data;
+  /**
+   * The path to the source file.
+   */
+  source: string;
+  /**
+   * The resource's destination path.
+   */
+  destination: string;
+  /**
+   * The resource's data.
+   */
+  data: string;
 
-  constructor(sourcePath: string, destinationPath?: string) {
-    this.source = sourcePath;
-    this.destination = getDestinationPath(destinationPath || sourcePath);
-    this.data = getFileData(sourcePath);
+  constructor(source: string, destination: string, data: string) {
+    this.source = source;
+    this.destination = getDestinationPath(destination);
+    this.data = data;
+  }
+
+  /**
+   * Create a resource from a source file's path.
+   *
+   * @param sourcePath The path of the source file.
+   * @param destinationPath The resource's destination path.
+   */
+  static createFromPath(sourcePath: string, destinationPath?: string) {
+    return new Resource(
+      sourcePath,
+      destinationPath || sourcePath,
+      getFileData(sourcePath)
+    );
   }
 }
--- test/SitemapTree.test.ts
+++ test/SitemapTree.test.ts
@@ -23,139 +23,149 @@ afterAll(() => {
 });
 
 describe('SitemapTree', () => {
-  describe('initialization', () => {
-    beforeEach(() => {
-      tree = new SitemapTree();
-    });
-    it('returns a null resource', () => {
+  describe('instantation', () => {
+    it('instantiates a new sitemap tree from arguments', () => {
+      tree = new SitemapTree(null, {});
+
       expect(tree!.resource).toBeNull();
-    });
-    it('returns a null parent', () => {
       expect(tree!.parent).toBeNull();
-    });
-    it('returns empty children', () => {
       expect(tree!.children).toEqual([]);
-    });
-    it('returns a null URL part', () => {
       expect(tree!.urlPart).toBeNull();
+      expect(tree!.url).toBeNull();
     });
-    it('returns a null URL', () => {
+    it('instantiates a new sitemap tree from a static method', () => {
+      tree = SitemapTree.create();
+
+      expect(tree!.resource).toBeNull();
+      expect(tree!.parent).toBeNull();
+      expect(tree!.children).toEqual([]);
+      expect(tree!.urlPart).toBeNull();
       expect(tree!.url).toBeNull();
     });
   });
-  describe('#add', () => {
-    beforeEach(() => {
-      tree = new SitemapTree();
+  describe('properties', () => {
+    describe('#siblings', () => {
+      beforeEach(() => {
+        tree = SitemapTree.create();
 
-      tree!.add(new Resource(path.normalize('path/first.txt')));
-      tree!.add(new Resource(path.normalize('path/second.txt')));
-    });
-    it('recursively adds the first level of the resource to the tree', () => {
-      const child = tree!.children[0];
+        tree!.add(Resource.createFromPath(path.normalize('path/first.txt')));
+        tree!.add(Resource.createFromPath(path.normalize('path/second.txt')));
+      });
+      it('returns all siblings at the same level', () => {
+        const [child] = tree!.children[0].children;
 
-      expect(child.resource).toBeNull();
-
-      expect(child.parent!.children).toHaveLength(1);
-      expect(child.parent!.resource).toBeNull();
-      expect(child.parent!.urlPart).toBeNull();
-      expect(child.parent!.url).toBeNull();
-
-      expect(child.children).toHaveLength(2);
-      expect(child.urlPart).toBe('path');
-      expect(child.url).toBe('path');
-    });
-    it('recursively adds the first child of the second level of the resource to the tree', () => {
-      const [child] = tree!.children[0].children;
-
-      expect(child.resource!.source).toBe(path.normalize('path/first.txt'));
-      expect(child.resource!.destination).toBe(path.normalize('/path/first/'));
-      expect(child.resource!.data).toBe('Some content.');
-
-      expect(child.parent!.children).toHaveLength(2);
-      expect(child.parent!.resource).toBeNull();
-      expect(child.parent!.urlPart).toBe('path');
-      expect(child.parent!.url).toBe('path');
-
-      expect(child.children).toEqual([]);
-      expect(child.urlPart).toBe('first');
-      expect(child.url).toBe(path.normalize('path/first'));
-    });
-    it('recursively adds the second child of the second level of the resource to the tree', () => {
-      const [, child] = tree!.children[0].children;
-
-      expect(child.resource!.source).toBe(path.normalize('path/second.txt'));
-      expect(child.resource!.destination).toBe(path.normalize('/path/second/'));
-      expect(child.resource!.data).toBe('Some other content.');
-
-      expect(child.parent!.children).toHaveLength(2);
-      expect(child.parent!.resource).toBeNull();
-      expect(child.parent!.urlPart).toBe('path');
-      expect(child.parent!.url).toBe('path');
-
-      expect(child.children).toEqual([]);
-      expect(child.urlPart).toBe('second');
-      expect(child.url).toBe(path.normalize('path/second'));
-    });
-  });
-  describe('#siblings', () => {
-    beforeEach(() => {
-      tree = new SitemapTree();
-
-      tree!.add(new Resource(path.normalize('path/first.txt')));
-      tree!.add(new Resource(path.normalize('path/second.txt')));
-    });
-    it('returns all siblings at the same level', () => {
-      const [child] = tree!.children[0].children;
-
-      expect(child.siblings).toHaveLength(2);
-    });
-    it('returns self from the top node', () => {
-      expect(tree!.siblings).toHaveLength(1);
-    });
-  });
-  describe('#fromUrl', () => {
-    beforeEach(() => {
-      tree = new SitemapTree();
-
-      tree!.add(new Resource(path.normalize('path/first.txt')));
-    });
-    it('returns sub-trees from URL', () => {
-      const subTree = tree!.fromUrl(path.normalize('path/first'));
-
-      expect(subTree.resource!.source).toBe(path.normalize('path/first.txt'));
-    });
-  });
-  describe('#fromResource', () => {
-    beforeEach(() => {
-      tree = new SitemapTree();
-    });
-    it('returns sub-trees from URL', () => {
-      const resource = new Resource(path.normalize('path/first.txt'));
-      tree!.add(resource);
-
-      const subTree = tree!.fromResource(resource);
-
-      expect(subTree.resource!.source).toBe(path.normalize('path/first.txt'));
-    });
-  });
-  describe('#allUrls', () => {
-    beforeEach(() => {
-      tree = new SitemapTree();
-
-      tree!.add(new Resource(path.normalize('path/first.txt')));
-      tree!.add(new Resource(path.normalize('path/second.txt')));
-    });
-    it('returns all URLs', () => {
-      expect(tree!.allUrls).toEqual({
-        path: expect.any(SitemapTree),
-        [path.normalize('path/first')]: expect.any(SitemapTree),
-        [path.normalize('path/second')]: expect.any(SitemapTree),
+        expect(child.siblings).toHaveLength(2);
+      });
+      it('returns self from the top node', () => {
+        expect(tree!.siblings).toHaveLength(1);
       });
     });
-    it('points to the same object in memory for all sub-trees', () => {
-      const subTree = tree!.fromUrl(path.normalize('path/first'));
+    describe('#allUrls', () => {
+      beforeEach(() => {
+        tree = SitemapTree.create();
 
-      expect(tree!.allUrls).toBe(subTree.allUrls);
+        tree!.add(Resource.createFromPath(path.normalize('path/first.txt')));
+        tree!.add(Resource.createFromPath(path.normalize('path/second.txt')));
+      });
+      it('returns all URLs', () => {
+        expect(tree!.allUrls).toEqual({
+          path: expect.any(SitemapTree),
+          [path.normalize('path/first')]: expect.any(SitemapTree),
+          [path.normalize('path/second')]: expect.any(SitemapTree),
+        });
+      });
+      it('points to the same object in memory for all sub-trees', () => {
+        const subTree = tree!.fromUrl(path.normalize('path/first'));
+
+        expect(tree!.allUrls).toBe(subTree.allUrls);
+      });
+    });
+  });
+  describe('methods', () => {
+    describe('#add', () => {
+      beforeEach(() => {
+        tree = SitemapTree.create();
+
+        tree!.add(Resource.createFromPath(path.normalize('path/first.txt')));
+        tree!.add(Resource.createFromPath(path.normalize('path/second.txt')));
+      });
+      it('recursively adds the first level of the resource to the tree', () => {
+        const child = tree!.children[0];
+
+        expect(child.resource).toBeNull();
+
+        expect(child.parent!.children).toHaveLength(1);
+        expect(child.parent!.resource).toBeNull();
+        expect(child.parent!.urlPart).toBeNull();
+        expect(child.parent!.url).toBeNull();
+
+        expect(child.children).toHaveLength(2);
+        expect(child.urlPart).toBe('path');
+        expect(child.url).toBe('path');
+      });
+      it('recursively adds the first child of the second level of the resource to the tree', () => {
+        const [child] = tree!.children[0].children;
+
+        expect(child.resource!.source).toBe(path.normalize('path/first.txt'));
+        expect(child.resource!.destination).toBe(
+          path.normalize('/path/first/')
+        );
+        expect(child.resource!.data).toBe('Some content.');
+
+        expect(child.parent!.children).toHaveLength(2);
+        expect(child.parent!.resource).toBeNull();
+        expect(child.parent!.urlPart).toBe('path');
+        expect(child.parent!.url).toBe('path');
+
+        expect(child.children).toEqual([]);
+        expect(child.urlPart).toBe('first');
+        expect(child.url).toBe(path.normalize('path/first'));
+      });
+      it('recursively adds the second child of the second level of the resource to the tree', () => {
+        const [, child] = tree!.children[0].children;
+
+        expect(child.resource!.source).toBe(path.normalize('path/second.txt'));
+        expect(child.resource!.destination).toBe(
+          path.normalize('/path/second/')
+        );
+        expect(child.resource!.data).toBe('Some other content.');
+
+        expect(child.parent!.children).toHaveLength(2);
+        expect(child.parent!.resource).toBeNull();
+        expect(child.parent!.urlPart).toBe('path');
+        expect(child.parent!.url).toBe('path');
+
+        expect(child.children).toEqual([]);
+        expect(child.urlPart).toBe('second');
+        expect(child.url).toBe(path.normalize('path/second'));
+      });
+    });
+    describe('#fromUrl', () => {
+      beforeEach(() => {
+        tree = SitemapTree.create();
+
+        tree!.add(Resource.createFromPath(path.normalize('path/first.txt')));
+      });
+      it('returns sub-trees from URL', () => {
+        const subTree = tree!.fromUrl(path.normalize('path/first'));
+
+        expect(subTree.resource!.source).toBe(path.normalize('path/first.txt'));
+      });
+    });
+    describe('#fromResource', () => {
+      beforeEach(() => {
+        tree = SitemapTree.create();
+      });
+      it('returns sub-trees from URL', () => {
+        const resource = Resource.createFromPath(
+          path.normalize('path/first.txt')
+        );
+        tree!.add(resource);
+
+        const subTree = tree!.fromResource(resource);
+
+        expect(subTree.resource!.source).toBe(path.normalize('path/first.txt'));
+      });
     });
   });
 });
